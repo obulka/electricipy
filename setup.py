c4f4bdd44e54014bffa43f8ae6002be2f7ddf81a
Owen Bulka <obulka@hotmail.com>
2021-10-16 19:32:31 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="pilectric",
+    version="0.1",
+    description="Raspberry Pi Device Control",
+    author="Owen Bulka",
+    packages=["pilectric"],
+    package_dir={"": "src"},
+)
